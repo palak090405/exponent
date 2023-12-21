a0f2b6b53d2b40caa70a270b321427453260c8f5
Palak <maurya.palak09@gmail.com>
2023-12-21 13:08:00 +0530

    Add exponent

---
+++ exponent.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+double power(double base, int exponent) {
+    double result = 1.0;
+
+    // Handle negative exponent
+    if (exponent < 0) {
+        base = 1 / base;
+        exponent = -exponent;
+    }
+
+    for (int i = 0; i < exponent; ++i) {
+        result *= base;
+    }
+
+    return result;
+}
+
+int main() {
+    double base;
+    int exponent;
+
+    printf("Enter the base: ");
+    scanf("%lf", &base);
+
+    printf("Enter the exponent: ");
+    scanf("%d", &exponent);
+
+    double result = power(base, exponent);
+
+    printf("%.2lf raised to the power of %d is: %.2lf\n", base, exponent, result);
+
+    return 0;
+}
